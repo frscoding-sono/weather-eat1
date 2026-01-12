7c444953ecc4ebb76b4b8865867686b4dac1f401
frscoding <frscoding@gmail.com>
2026-01-12 14:55:01 +0000

    feat: 나노바나나 v18.0 - AI 제네시스 백업

---
+++ backup_v18_banana_background/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Weather from './Weather.svelte';
+  import './app.css';
+</script>
+
+<div id="app" class="grid grid-cols-2 grid-rows-2 h-screen p-8 gap-8 box-border">
+  <main class="col-span-2 row-span-2 flex justify-center items-center">
+    <Weather />
+  </main>
+</div>
